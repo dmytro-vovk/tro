448ca306399525b5d05d5755479b3646859a0944
Dmytro Vovk <dmitry.vovk@gmail.com>
2022-03-28 22:39:20 +0100

    Cleanup front build script

--- scripts/build-front.sh
+++ scripts/build-front.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+set -eu
+
 docker run \
   --rm \
   --volume $(PWD):/src \
   --workdir /src \
   node:alpine3.14 \
-  npm install && \
+  npm install
+
 docker run \
   --rm \
   --volume $(PWD):/src \
   --workdir /src \
   node:alpine3.14 \
-  npm run build && \
-rm -rf ./internal/webserver/handlers/home/css && \
-cp -rf ./frontend/styles ./internal/webserver/handlers/home/css && \
-gzip -c ./frontend/index.html > ./internal/webserver/handlers/home/index.html.gz && \
-gzip -f ./internal/webserver/handlers/home/index.js && \
+  npm run build
+
+rm -rf ./internal/webserver/handlers/home/css
+
+cp -rf ./frontend/styles ./internal/webserver/handlers/home/css
+
+gzip -c ./frontend/index.html > ./internal/webserver/handlers/home/index.html.gz
+
+gzip -f ./internal/webserver/handlers/home/index.js
+
 gzip -f ./internal/webserver/handlers/home/index.js.map
